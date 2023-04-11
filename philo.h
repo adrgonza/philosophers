08d2d5131f392bc0deaa1fef500723facb8eb5f5
adrgonza <105714518+adriiiiiiiii@users.noreply.github.com>
2023-04-11 02:40:00 +0200

    v0.8

--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: adrgonza <adrgonza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 00:45:46 by adrgonza          #+#    #+#             */
-/*   Updated: 2023/04/10 17:19:07 by adrgonza         ###   ########.fr       */
+/*   Updated: 2023/04/11 02:03:00 by adrgonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,17 +25,20 @@ typedef struct philo{
 	pthread_mutex_t	*left_fork;
 	pthread_mutex_t	*right_fork;
 	int				id;
-	int 			*bang;
+	int 			*start;
 	int				time_to_die;
 	int				time_to_eat;
 	int				time_sleep;
 	int				nb_times_philos_eat;
+	int				times_eaten;
+	int				alredy_eaten;
 	long long		last_time_eat;
 	long long		star_time;
 }	t_philo;
 
 typedef struct data{
 	int 			number_of_philosophers;
+	int				start_race;
 	long long		start_time;
 	pthread_mutex_t	*forks;
 	pthread_t		*philosophers;
--- philosophers.c
+++ philosophers.c
@@ -6,78 +6,85 @@
 /*   By: adrgonza <adrgonza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 00:27:45 by adrgonza          #+#    #+#             */
-/*   Updated: 2023/04/10 17:27:25 by adrgonza         ###   ########.fr       */
+/*   Updated: 2023/04/11 02:38:27 by adrgonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
 /*
--implement mutex who protects printf
--implement times eaten
 -implement better death
 -fix leaks and pthreads problems
-
 */
 
-int print_philos(t_philo *philo, char *str)
+void ft_paradise(t_data *data)
 {
-	long long act_time;
-	//lock mutex
-	act_time = timex() - philo->star_time;
-	if (act_time - philo->last_time_eat > philo->time_to_die)
+	long long	act_time;
+	int			nb_philos_eaten;
+	int			i;
+
+	nb_philos_eaten = 0;
+	while (1) /* check if there is a dead philo  */
 	{
-		printf("%lldms philo %d has died\n", act_time, philo->id);
-		exit(0);
+		i = -1;
+		while (++i < data->number_of_philosophers) /* iterate every thread */
+		{
+			act_time = timex() - data->philo->star_time;
+			if (act_time - data->philo[i].last_time_eat > data->philo[i].time_to_die) /* check if in that thread the philosopher died */
+			{
+				printf("%lldms philo %d has died\n", act_time, data->philo[i].id);
+				exit(0);
+			}
+			if ((data->philo[i].times_eaten == data->philo[i].nb_times_philos_eat) && data->philo[i].alredy_eaten == 0)
+			{
+				nb_philos_eaten++;
+				data->philo[i].alredy_eaten = 1;
+				if (nb_philos_eaten == data->number_of_philosophers)
+					exit(0);
+			}
+		}
 	}
-	//si algiuno
-	printf("%lldms philo %d %s", act_time, philo->id, str);
-	//unlock the mutex
-	return (0);
 }
 
 void	*philo_actions(void	*arg)
 {
 	t_philo *philo;
 
-	philo = arg;
-	while (!philo->bang);
-	if ((philo->id % 2) == 0)
+	philo = arg; /* give the value of arg to philo */
+	while (philo->start == 0); /* make all the philos wait for beeing all created */
+	if ((philo->id % 2) == 0) /* checks if the philo is pair to give him delay */
 		usleep(50);
-	philo->last_time_eat = timex() - philo->star_time;
-	while(1)
+	philo->last_time_eat = 0;
+	while (1)
 	{
 		pthread_mutex_lock(philo->left_fork); /* lock left fork */
-		print_philos(philo, "has taken a fork\n"); /* take left fork */
+		printf("%lldms philo %d has taken a fork\n", timex() - philo->star_time, philo->id); /* take left */
 		pthread_mutex_lock(philo->right_fork); /* lock right fork */
-		print_philos(philo, "has taken a fork\n"); /* take right fork */
+		printf("%lldms philo %d has taken a fork\n", timex() - philo->star_time, philo->id); /* take right fork */
 		philo->last_time_eat = timex() - philo->star_time;
-		print_philos(philo, "is eating\n"); /* print is eating */
+		printf("%lldms philo %d is eating\n", timex() - philo->star_time, philo->id); /* print is eating */
+		if (philo->times_eaten != -1)
+			philo->times_eaten++;
 		usleep(philo->time_to_eat * 1000); /* wait the time to eat */
 		pthread_mutex_unlock(philo->left_fork); /* release left fork */
 		pthread_mutex_unlock(philo->right_fork); /* release right fork */
-		print_philos(philo, "is sleeping\n"); /* print is sleeping */
+		printf("%lldms philo %d is sleeping\n", timex() - philo->star_time, philo->id); /* print is sleeping */
 		usleep(philo->time_sleep * 1000); /* wait time for sleep */
-		print_philos(philo, "is thinking\n"); /* print is thinking */
+		printf("%lldms philo %d is thinking\n", timex() - philo->star_time, philo->id); /* print is thinking */
 	}
-	return (NULL);
 }
 
-int create_pthread(t_data	*data, int argc, char **argv)
+void	create_pthread(t_data	*data, int argc, char **argv)
 {
-	int bang; /* makes start al threads at the same time */
-	int i;
+	int	i;
 
-    data->start_time = timex();  /* get the time at program start */
-	bang = 0;
+	data->start_time = timex();  /* get the time at program start */
+	data->start_race = 0;
 	i = -1;
-	while (++i < data->number_of_philosophers)	/* have to create every pthread and put in the philo structure*/
+	while (++i < data->number_of_philosophers) /* have to create every pthread and put in the philo structure*/
 	{
 		if (pthread_mutex_init(&(data->forks[i]), NULL)) /* mutex creation */
-			return (0);
-		data->philo[i].nb_times_philos_eat = -1; /* times philo has to eat inicied at -1 */
-		if (argc == 6)
-			data->philo[i].nb_times_philos_eat = ft_atoi(argv[5]); /* if there is value put it in the variable */
+			exit(0);
 		data->philo[i].star_time = data->start_time; /* star time writted on data give it to philo */
 		data->philo[i].id = i + 1; /* philo id */
 		data->philo[i].right_fork = &(data->forks[i]); /* putting every fork in a fork variable */
@@ -85,18 +92,22 @@ int create_pthread(t_data	*data, int argc, char **argv)
 		data->philo[i].time_to_die = ft_atoi(argv[2]);
 		data->philo[i].time_to_eat = ft_atoi(argv[3]);
 		data->philo[i].time_sleep = ft_atoi(argv[4]);
-		data->philo[i].bang = &bang; /* gives the acces to the same variable to all philos */
+		data->philo[i].nb_times_philos_eat = -1; /* times philo has to eat inicied at -1 */
+		if (argc == 6)
+			data->philo[i].nb_times_philos_eat = ft_atoi(argv[5]); /* if there is value put it in the variable */
+		data->philo[i].alredy_eaten = 0;
+		data->philo[i].start = &data->start_race; /* gives the acces to the same variable to all philos */
 		pthread_create(&(data->philosophers[i]), NULL, philo_actions, &(data->philo[i])); /* creating every pthread */
 	}
-	bang = 1; /* al threads starts */
-	while (1);
+	data->start_race = 1; /* al threads starts */
+	ft_paradise(data);
 }
 
-int main (int argc, char **argv)
+int	main(int argc, char **argv)
 {
 	t_data	data;
-	int i;
-	int j;
+	int		i;
+	int		j;
 
 	if (argc < 5 || argc > 6) /* check there is only five or six arguments */
 		return (0);
@@ -116,6 +127,7 @@ int main (int argc, char **argv)
 	if (!data.forks)
 		return (0);
 	data.philosophers = malloc(sizeof(pthread_t) * (data.number_of_philosophers)); /* allocate memory for threads */
-	if(!data.philosophers || !create_pthread(&data, argc, argv))
-		return(0);
+	if (!data.philosophers)
+		return (0);
+	create_pthread(&data, argc, argv);
 }
